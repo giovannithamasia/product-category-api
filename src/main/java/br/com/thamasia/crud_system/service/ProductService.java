2df3eecf6bc75a1e9cd8e3b15be61ef9db9d95a1
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2026-04-09 19:07:38 -0300

    feat: implements ProductService with business logic and category validation

---
+++ src/main/java/br/com/thamasia/crud_system/service/ProductService.java
@@ -0,0 +1,81 @@
+package br.com.thamasia.crud_system.service;
+
+import br.com.thamasia.crud_system.dto.ProductDto;
+import br.com.thamasia.crud_system.dto.ProductResponseDto;
+import br.com.thamasia.crud_system.exception.CategoryNotFoundException;
+import br.com.thamasia.crud_system.exception.ProductNotFoundException;
+import br.com.thamasia.crud_system.model.Category;
+import br.com.thamasia.crud_system.model.Product;
+import br.com.thamasia.crud_system.repository.CategoryRepository;
+import br.com.thamasia.crud_system.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    public void registerProduct(ProductDto dto){
+        Category category = categoryRepository.findById(dto.getIdCategory())
+                .orElseThrow(() -> new CategoryNotFoundException("Category not found"));
+
+        Product product = new Product();
+
+        product.setNameProduct(dto.getNameProduct());
+        product.setPrice(dto.getPrice());
+        product.setCategory(category);
+
+        productRepository.save(product);
+    }
+
+    public List<ProductResponseDto> listProducts(){
+        return productRepository.findAll()
+                .stream()
+                .map(ProductResponseDto::toProductResponseDto)
+                .toList();
+    }
+
+    public ProductResponseDto searchById(Long idCategory){
+        Product product = productRepository.findById(idCategory)
+                .orElseThrow(() -> new ProductNotFoundException("Product not found"));
+
+        return ProductResponseDto.toProductResponseDto(product);
+    }
+
+    public List<ProductResponseDto> listProductsByCategory(Long id){
+        categoryRepository.findById(id)
+                .orElseThrow(() -> new CategoryNotFoundException("Category not found"));
+
+        List<Product> product = productRepository.findByCategoryIdCategory(id);
+
+       return product.stream()
+                .map(ProductResponseDto::toProductResponseDto)
+                .toList();
+    }
+
+    public void updateProduct(Long idProduct,ProductDto dto){
+        Product product = productRepository.findById(idProduct)
+                .orElseThrow(() -> new ProductNotFoundException("Product not found"));
+
+        Category category = categoryRepository.findById(dto.getIdCategory())
+                .orElseThrow(() -> new CategoryNotFoundException("Category not found"));
+
+        product.setNameProduct(dto.getNameProduct());
+        product.setPrice(dto.getPrice());
+        product.setCategory(category);
+
+        productRepository.save(product);
+    }
+
+    public void deleteProduct(Long idProduct){
+        productRepository.findById(idProduct)
+                .orElseThrow(() -> new ProductNotFoundException("Product not found"));
+
+        productRepository.deleteById(idProduct);
+    }
+}
