9fe8e0d8ce15c1599a2f23b4ceda2dca1145f885
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2026-04-10 18:30:58 -0300

    feat: creates Product and Product ResponseDto for API data transfer

---
+++ src/main/java/br/com/thamasia/crud_system/dto/ProductDto.java
@@ -0,0 +1,28 @@
+package br.com.thamasia.crud_system.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDto {
+
+    @NotBlank(message = "The product name is required")
+    @Size(min = 2,max = 100,message = "The name must be between two and one hundred characters long")
+    private String nameProduct;
+
+    @Positive(message = "The price cannot be negative or zero")
+    @NotNull(message = "The price cannot be zero")
+    private BigDecimal price;
+
+    @NotNull(message = "The category id cannot be null")
+    private Long idCategory;
+}
