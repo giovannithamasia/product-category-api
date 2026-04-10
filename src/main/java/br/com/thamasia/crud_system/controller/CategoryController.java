79c50fdd2e7170009e057c0c3087f63d4af28b2e
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2026-04-10 19:19:33 -0300

    feat: implements GenericController to generate a location header when creating categories

--- src/main/java/br/com/thamasia/crud_system/controller/CategoryController.java
+++ src/main/java/br/com/thamasia/crud_system/controller/CategoryController.java
@@ -2,6 +2,7 @@ package br.com.thamasia.crud_system.controller;
 
 import br.com.thamasia.crud_system.dto.CategoryDto;
 import br.com.thamasia.crud_system.dto.CategoryResponseDto;
+import br.com.thamasia.crud_system.model.Category;
 import br.com.thamasia.crud_system.service.CategoryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -9,19 +10,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
 @RequiredArgsConstructor
-public class CategoryController {
+public class CategoryController implements GenericController{
 
     private final CategoryService service;
 
     @PostMapping
     public ResponseEntity<Void> registerCategory(@RequestBody @Valid CategoryDto dto) {
-        service.registerCategory(dto);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        Category category = service.registerCategory(dto);
+
+        URI location = generateHeaderLocation(category.getIdCategory());
+
+        return ResponseEntity.created(location).build();
     }
 
     @GetMapping
