1c3eac523b0f0a3bf011fa3f26def190cfc82cae
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2026-04-10 18:18:57 -0300

    feat: implements the Product model and the Product Repository with category relationships

---
+++ src/main/java/br/com/thamasia/crud_system/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package br.com.thamasia.crud_system.repository;
+
+import br.com.thamasia.crud_system.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    List<Product> findByCategoryIdCategory(Long idCategory);
+}
