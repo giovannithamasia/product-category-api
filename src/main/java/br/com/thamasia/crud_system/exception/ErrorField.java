0b466f399fe1fe9f58bb307938a56e5fa4bd295f
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2026-04-10 15:16:12 -0300

    feat: implements safe handling of global exceptions and support for bean validation

---
+++ src/main/java/br/com/thamasia/crud_system/exception/ErrorField.java
@@ -0,0 +1,14 @@
+package br.com.thamasia.crud_system.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorField {
+
+    private String field;
+    private String error;
+}
--- src/main/java/br/com/thamasia/crud_system/exception/ErrorResponse.java
+++ src/main/java/br/com/thamasia/crud_system/exception/ErrorResponse.java
@@ -1,17 +1,21 @@
 package br.com.thamasia.crud_system.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
 
     private int status;
     private String message;
-
+    private List<ErrorField> errors;
 }
